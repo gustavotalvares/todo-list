af76749ad11f7808a152b586e2b9ab100d41226f
Gustavo Alvares <alvaresgustavo01@gmail.com>
2023-09-07 14:31:25 -0300

    Constroi layout inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+      colors: {
+        coolors: {
+          blue: {
+            100: '#CAF0F8',
+            200: '#90E0EF',
+            300: '#00B4D8',
+            400: '#0077B6',
+            500: '#03045E',
+          },
+        },
+      }
     },
   },
   plugins: [],
